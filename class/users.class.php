c3ceeec90dc5aec5db3e439313cfcdd821fcc22c
German Dario Erazo <german.erazo@gmail.com>
2025-04-22 00:30:42 -0500

    validacion y actualizacion de token

--- class/users.class.php
+++ class/users.class.php
@@ -183,6 +183,15 @@ class users extends connection {
         $query = "SELECT * FROM users_token WHERE token = '$this->token' AND state = 0";
         $data = parent::getData($query);
         if($data) {
+            $tokenDate = strtotime($data[0]['date']); // campo 'date' del token
+            $now = time();
+            $diffHours = ($now - $tokenDate) / 3600;
+
+            if ($diffHours > 12) {
+                // Expirar el token porque pasaron más de 12 horas
+                $this->updateToken($data[0]['tokenId']);
+                return 0; // Token ya no es válido
+            }
             return $data;
         } else {
             return 0;
@@ -191,7 +200,7 @@ class users extends connection {
 
     private function updateToken($tokenid) {
         $date = date("Y-m-d H:i:s");
-        $query = "UPDATE usuarios_token SET date = '$date' WHERE tokenId = '$tokenid'";
+        $query = "UPDATE usuarios_token SET state = 1, date = '$date' WHERE tokenId = '$tokenid'";
         $response = parent::nonQuery($query);
         if ($response >= 1) {
             return $response;
