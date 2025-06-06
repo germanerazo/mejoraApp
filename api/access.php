c897c847e29a5032b638d6367714dd49b70c66b0
German Dario Erazo <german.erazo@gmail.com>
2025-06-05 22:13:51 -0500

    ajuste access api

--- api/access.php
+++ api/access.php
@@ -11,6 +11,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 // Permitir cabeceras personalizadas
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
