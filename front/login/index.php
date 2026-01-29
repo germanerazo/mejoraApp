bae246ac2f5e23ba7ea3ab7de8b5e45acf1ce5e1
German Dario Erazo <german.erazo@gmail.com>
2026-01-28 21:43:27 -0500

    configuracion para app.mejora.com.co

---
+++ front/login/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Index file - Redirects to login.php
+ * This file serves as the entry point for app.mejora.com.co
+ */
+
+// Redirect to login.php
+header('Location: login.php');
+exit();
