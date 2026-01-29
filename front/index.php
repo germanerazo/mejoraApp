4743272bbb7ae86a5e3643f1cd9cc6e3415d789f
German Dario Erazo <german.erazo@gmail.com>
2026-01-28 22:19:20 -0500

    configuracion para app.mejora.com.co

---
+++ front/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Este archivo debe estar en repositories/mejoraApp/front/index.php
+ * Redirige al usuario a la carpeta login para que las rutas ../ funcionen
+ */
+header("Location: login/login.php");
+exit;
