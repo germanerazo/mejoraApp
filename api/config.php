61aa9d8df8ae56b6ec5d60635e5e2bf57cd623ef
German Dario Erazo <german.erazo@gmail.com>
2026-03-03 00:11:44 -0500

    config CORS

--- api/config.php
+++ api/config.php
@@ -1,4 +1,14 @@
 <?php
 // Configuración global de la aplicación
 date_default_timezone_set('America/Bogota');
+
+// CORS configuration
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Origin, Accept");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 ?>
