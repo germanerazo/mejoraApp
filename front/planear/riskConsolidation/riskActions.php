0a29631a9ba6ec4f5b03aa41456ff39e431f354e
German Dario Erazo <german.erazo@gmail.com>
2026-05-14 22:21:05 -0500

    ajuste estilos programas de gestion

--- front/planear/riskConsolidation/riskActions.php
+++ front/planear/riskConsolidation/riskActions.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title>Programa de Gestión</title>
     <!-- Common CSS -->
-    <link rel="stylesheet" href="/mejoraApp/front/styles/colors.css">
-    <link rel="stylesheet" href="/mejoraApp/front/planear/risk/risk.css?v=1.0">
+    <link rel="stylesheet" href="../styles/colors.css">
+    <link rel="stylesheet" href="../planear/risk/risk.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
